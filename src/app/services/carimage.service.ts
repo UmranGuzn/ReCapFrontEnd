5e524c75fd1ec83f23860a21f0a6f92fe70b2efd
UmranGuzn <umran.guzn@gmail.com>
2021-04-07 21:42:31 +0300

    CarImage Properties Added

---
+++ src/app/services/carimage.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from "src/environments/environment";
+import { CarImage } from '../models/carImage';
+import { ListResponseModel } from '../models/listResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarimageService {
+
+  apiUrl=environment.url;
+  
+  constructor(private httpClient:HttpClient) { }
+
+  getCarImages():Observable<ListResponseModel<CarImage>>{
+    let newPath=this.apiUrl+"carimages/getall";
+    return this.httpClient.get<ListResponseModel<CarImage>>(newPath);
+  }
+
+  getCarImageByCarId(carId:number):Observable<ListResponseModel<CarImage>>{
+    let newPath=this.apiUrl+"carimages/getcarimagesbycarid?carId="+carId;
+
+    return this.httpClient.get<ListResponseModel<CarImage>>(newPath);
+  }
+}
