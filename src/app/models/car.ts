b27bbc388ca5df89dcbc4f629bfbfc3ec293a8c3
UmranGuzn <umran.guzn@gmail.com>
2021-04-07 21:43:52 +0300

    Models Updated

--- src/app/models/car.ts
+++ src/app/models/car.ts
@@ -1,4 +1,8 @@
 export interface Car{
 	carId:number,
-	brandId:number
+	brandId:number,
+	colorId:number,
+	modelYear:string,
+	dailyPrice:number,
+	description:string
 }
--- src/app/models/carDetail.ts
+++ src/app/models/carDetail.ts
@@ -1,8 +1,11 @@
 export interface CarDetail{
 	carId:number;
+	colorId:number;
+	brandId:number;
 	brandName:string;
 	colorName:string;
 	dailyPrice:number;
 	descriptions:string;
 	modelYear:string;
+	imagePath:string;
 }
